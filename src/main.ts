4a65adf618c7afab360596934cbf4600c23ae0f9
gordonmenzies <heymish96@gmail.com>
2024-03-14 16:39:31 +0000

    basic html content for screen 3 gameplay added

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,8 @@ if (screenSelect === 0) {
 } else if (screenSelect === 2) {
   app.innerHTML = "SCREEN 2 LOG IN ";
 } else if (screenSelect === 3) {
-  app.innerHTML = "Screen 3 GAMEPLAY ";
+  console.log("SCREEN 3 GAMEPLAY");
+  app.innerHTML = 
 }
 
 /*
@@ -66,3 +67,57 @@ function checkAPICall() {
 }
 
 button.addEventListener("click", checkAPICall);
+
+const screenThreeRender: string = `      
+<section class="background__container">
+<!-- LEFT HAND SIDE COLUMN TO GO HERE  -->
+<section class="timerScore__container">
+  <div class="userName"></div>
+  <div class="timer"></div>
+</section>
+<!-- QUESTION AND ANSWER GROUP -->
+<section class="questionAndAnswer__container">
+  <div class="question__div">
+    <h1>QUESTION TEXT</h1>
+  </div>
+  <div class="answer__divContainer">
+    <div class="answer__div">
+      <h1 class="answer__text">ANSWER 1</h1>
+    </div>
+    <div class="answer__div">
+      <h1 class="answer__text">ANSWER 2</h1>
+    </div>
+    <div class="answer__div">
+      <h1 class="answer__text">ANSWER 3</h1>
+    </div>
+    <div class="answer__div">
+      <h1 class="answer__text">ANSWER 4</h1>
+    </div>
+  </div>
+</section>
+<!-- MONEY TREE  -->
+<section class="moneyTree__container">
+  <div class="lifeLine__Container">
+    <div class="lineline"></div>
+    <div class="lineline"></div>
+    <div class="lineline"></div>
+  </div>
+  <div class="moneytree__textContainer">
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+    <h2 class="moneyTree__text"></h2>
+  </div>
+</section>
+</section>`;
