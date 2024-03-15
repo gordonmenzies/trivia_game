3432d992279bf248c54fac7ca28dd2157435f878
gordonmenzies <heymish96@gmail.com>
2024-03-15 16:38:59 +0000

    start game and next question test feature added, answers can be selected and questions and answers can be populated

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,38 @@
 import "./style.scss";
+import { Question } from "./questionType";
 
-/* Query Selectors
+/* Testing Buttons
  */
-const app = document.querySelector<HTMLDivElement>("#app");
 
 const button = document.querySelector<HTMLDivElement>("#apiCall");
 const nextQuestionButton =
   document.querySelector<HTMLDivElement>("#nextQuestion");
 
-const question = document.querySelector<HTMLHeadingElement>(".question__text");
-const answers = document.querySelectorAll<HTMLHeadingElement>(".answer__text");
+/*
+  Questions and Answer Contatiners
+*/
 
-if (!app || !button || !nextQuestionButton) {
+const question = document.querySelector<HTMLHeadingElement>(".question__text");
+const answerArray =
+  document.querySelectorAll<HTMLHeadingElement>(".answer__text");
+
+/*
+  Answer Buttons
+*/
+
+if (!button || !nextQuestionButton) {
   throw new Error("there is an error with the selector");
 }
 
-if (!question || !answers) {
+if (!question || !answerArray) {
   throw new Error("there is an error with the questions or answers");
 }
 
 /* Global Variables
  */
-let screenSelect = 0;
 let questionIndex = 0;
 
-/*
-  API call to get questions 
-*/
-
-let questions: Object;
+let questionArray: Question[] = [];
 
 // Define the API URL
 let apiBuiltUrl = "https://opentdb.com/api.php?amount=12&type=multiple";
@@ -44,275 +48,70 @@ async function APICall(apiUrl: string) {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
-      questions = data;
+      console.log("data", data);
+      data.results.forEach((question: Question) => {
+        questionArray.push(question);
+      });
     })
     .catch((error) => {
       console.error("Error:", error);
     });
 
-  return questions;
+  return questionArray;
 }
 
 APICall(apiBuiltUrl);
+
+const populateQuestionsAndAnswers = (sortedAnswers: string[]): void => {
+  question.innerHTML = questionArray[questionIndex].question;
+
+  for (let i: number = 0; i < sortedAnswers.length; i++) {
+    answerArray[i].innerHTML = sortedAnswers[i];
+  }
+  console.log("populateQuestionsAndAnswers");
+};
+
+const sortAnswers = (question: Question) => {
+  let answerArray: string[] = [];
+  answerArray.push(question.correct_answer);
+  question.incorrect_answers.forEach((answer: string) => {
+    answerArray.push(answer);
+  });
+  answerArray.sort();
+  return answerArray;
+};
+
+const startGame = () => {
+  populateQuestionsAndAnswers(sortAnswers(questionArray[questionIndex]));
+  questionIndex = questionIndex + 1;
+};
+
+const nextQuestion = () => {
+  populateQuestionsAndAnswers();
+  questionIndex = questionIndex + 1;
+};
+
+const answerSelected = (event: Event) => {
+  const button = event.currentTarget as HTMLDivElement;
+  if (button.innerHTML === questionArray[questionIndex].correct_answer) {
+    console.log("correct answer");
+    questionIndex = questionIndex + 1;
+  } else {
+    console.log("incorrect answer ");
+  }
+};
 
 /*
   Event Listeners 
 */
 
-button.addEventListener("click", checkAPICall);
+button.addEventListener("click", startGame);
 nextQuestionButton.addEventListener("click", nextQuestion);
 
-const renderScreen = (screenSelect: number): void => {
-  if (screenSelect === 0) {
-    app.innerHTML = "SCREEN 0 WElCOME";
-  } else if (screenSelect === 1) {
-    app.innerHTML = "SCREEN 1 GAME SELECT ";
-  } else if (screenSelect === 2) {
-    app.innerHTML = "SCREEN 2 LOG IN ";
-  } else if (screenSelect === 3) {
-    console.log("SCREEN 3 GAMEPLAY");
-    app.innerHTML = screenThreeRender;
-  }
-};
-
-let screenThreeRender: string = `      
-<section class="background__container">
-<!-- LEFT HAND SIDE COLUMN TO GO HERE  -->
-<section class="timerScore__container">
-  <div class="userName"></div>
-  <div class="timer"></div>
-</section>
-<!-- QUESTION AND ANSWER GROUP -->
-<section class="questionAndAnswer__container">
-  <div class="question__div">
-    <h1>QUESTION TEXT</h1>
-  </div>
-  <div class="answer__divContainer">
-    <div class="answer__div">
-      <h1 class="answer__text">ANSWER 1</h1>
-    </div>
-    <div class="answer__div">
-      <h1 class="answer__text">ANSWER 2</h1>
-    </div>
-    <div class="answer__div">
-      <h1 class="answer__text">ANSWER 3</h1>
-    </div>
-    <div class="answer__div">
-      <h1 class="answer__text">ANSWER 4</h1>
-    </div>
-  </div>
-</section>
-<!-- MONEY TREE  -->
-<section class="moneyTree__container">
-  <div class="lifeLine__Container">
-    <div class="lineline"></div>
-    <div class="lineline"></div>
-    <div class="lineline"></div>
-  </div>
-  <div class="moneytree__textContainer">
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-    <h2 class="moneyTree__text"></h2>
-  </div>
-</section>
-<section class="background__container">
-<!-- LEFT HAND SIDE COLUMN TO GO HERE  -->
-<!-- <section class="timerScore__container">
-  <div class="userName">Username</div>
-  <div class="timer">Timer</div>
-</section> -->
-<!-- QUESTION AND ANSWER GROUP -->
-<section class="questionAndAnswer__container">
-  <div class="question__container">
-    <h1>QUESTION TEXT</h1>
-  </div>
-  <div class="answer__container">
-    <h1 class="answer__text">ANSWER 1</h1>
-    <h1 class="answer__text">ANSWER 2</h1>
-    <h1 class="answer__text">ANSWER 3</h1>
-    <h1 class="answer__text">ANSWER 4</h1>
-  </div>
-</section>
-<!-- MONEY TREE  -->
-<section class="moneyTree__container">
-  <div class="lifeline__container">
-    <img class="lifeline__img" src='./src/images/phoneafriendwebp.webp'></img>
-    <img class="lifeline__img" src='./src/images/asktheaudience.webp'></img>
-    <img class="lifeline__img" src='./src/images/5050.webp'></img>
-  </div>
-  <div class="moneytree__textContainer">
-    <h2 class="moneyTree__text">15 1,000,000</h2>
-    <h2 class="moneyTree__text">14 500,000</h2>
-    <h2 class="moneyTree__text">13 250,000</h2>
-    <h2 class="moneyTree__text">12 125,000</h2>
-    <h2 class="moneyTree__text">11 64,000</h2>
-    <h2 class="moneyTree__text">10 32,000</h2>
-    <h2 class="moneyTree__text">9 16,000</h2>
-    <h2 class="moneyTree__text">8 8000</h2>
-    <h2 class="moneyTree__text">7 4000</h2>
-    <h2 class="moneyTree__text">6 2000</h2>
-    <h2 class="moneyTree__text">5 1000</h2>
-    <h2 class="moneyTree__text">4 500</h2>
-    <h2 class="moneyTree__text">3 300</h2>
-    <h2 class="moneyTree__text">2 200</h2>
-    <h2 class="moneyTree__text">1 100</h2>
-  </div>
-</section>
-</section>`;
+answerArray.forEach((button) => {
+  button.addEventListener("click", answerSelected);
+});
 
 /*
   TEST CODE
 */
-
-const sortQuestions = (APIResults: Object) => {
-  const questionArray: Object[] = [];
-  const easyQuestions: String[] = APIResults.results.filter(
-    (question: Object) => {
-      return question.difficulty === "easy";
-    }
-  );
-  const mediumQuestions: String[] = APIResults.results.filter(
-    (question: Object) => {
-      return question.difficulty === "medium";
-    }
-  );
-  const hardQuestions: String[] = APIResults.results.filter(
-    (question: Object) => {
-      return question.difficulty === "easy";
-    }
-  );
-  easyQuestions.forEach((question) => {
-    questionArray.push(question);
-  });
-  mediumQuestions.forEach((question) => {
-    questionArray.push(question);
-  });
-  hardQuestions.forEach((question) => {
-    questionArray.push(question);
-  });
-  return questionArray;
-};
-
-// sort answers
-const sortAnswers = (arrayOfQuestions: Object[]) => {
-  arrayOfQuestions.forEach((question) => {});
-};
-
-const nextQuestion = (arrayOfQuestions: Object[], questionIndex: number) => {
-  screenThreeRender = `<section class="background__container">
-  <!-- LEFT HAND SIDE COLUMN TO GO HERE  -->
-  <section class="timerScore__container">
-    <div class="userName"></div>
-    <div class="timer"></div>
-  </section>
-  <!-- QUESTION AND ANSWER GROUP -->
-  <section class="questionAndAnswer__container">
-    <div class="question__div">
-      <h1>${arrayOfQuestions[questionIndex].question}</h1>
-    </div>
-    <div class="answer__divContainer">
-      <div class="answer__div">
-        <h1 class="answer__text">ANSWER 1</h1>
-      </div>
-      <div class="answer__div">
-        <h1 class="answer__text">ANSWER 2</h1>
-      </div>
-      <div class="answer__div">
-        <h1 class="answer__text">ANSWER 3</h1>
-      </div>
-      <div class="answer__div">
-        <h1 class="answer__text">ANSWER 4</h1>
-      </div>
-    </div>
-  </section>
-  <!-- MONEY TREE  -->
-  <section class="moneyTree__container">
-    <div class="lifeLine__Container">
-      <div class="lineline"></div>
-      <div class="lineline"></div>
-      <div class="lineline"></div>
-    </div>
-    <div class="moneytree__textContainer">
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-      <h2 class="moneyTree__text"></h2>
-    </div>
-  </section>
-  <section class="background__container">
-  <!-- LEFT HAND SIDE COLUMN TO GO HERE  -->
-  <!-- <section class="timerScore__container">
-    <div class="userName">Username</div>
-    <div class="timer">Timer</div>
-  </section> -->
-  <!-- QUESTION AND ANSWER GROUP -->
-  <section class="questionAndAnswer__container">
-    <div class="question__container">
-      <h1>QUESTION TEXT</h1>
-    </div>
-    <div class="answer__container">
-      <h1 class="answer__text">ANSWER 1</h1>
-      <h1 class="answer__text">ANSWER 2</h1>
-      <h1 class="answer__text">ANSWER 3</h1>
-      <h1 class="answer__text">ANSWER 4</h1>
-    </div>
-  </section>
-  <!-- MONEY TREE  -->
-  <section class="moneyTree__container">
-    <div class="lifeline__container">
-      <img class="lifeline__img" src='./src/images/phoneafriendwebp.webp'></img>
-      <img class="lifeline__img" src='./src/images/asktheaudience.webp'></img>
-      <img class="lifeline__img" src='./src/images/5050.webp'></img>
-    </div>
-    <div class="moneytree__textContainer">
-      <h2 class="moneyTree__text">15 1,000,000</h2>
-      <h2 class="moneyTree__text">14 500,000</h2>
-      <h2 class="moneyTree__text">13 250,000</h2>
-      <h2 class="moneyTree__text">12 125,000</h2>
-      <h2 class="moneyTree__text">11 64,000</h2>
-      <h2 class="moneyTree__text">10 32,000</h2>
-      <h2 class="moneyTree__text">9 16,000</h2>
-      <h2 class="moneyTree__text">8 8000</h2>
-      <h2 class="moneyTree__text">7 4000</h2>
-      <h2 class="moneyTree__text">6 2000</h2>
-      <h2 class="moneyTree__text">5 1000</h2>
-      <h2 class="moneyTree__text">4 500</h2>
-      <h2 class="moneyTree__text">3 300</h2>
-      <h2 class="moneyTree__text">2 200</h2>
-      <h2 class="moneyTree__text">1 100</h2>
-    </div>
-  </section>
-  </section>`;
-};
-
-function checkAPICall() {
-  let arrayOfQuestions: Object = sortQuestions(questions);
-  console.log(questions.results);
-  sortQuestions(arrayOfQuestions);
-  sortAnswers(arrayOfQuestions);
-  nextQuestion(arrayOfQuestions);
-}
